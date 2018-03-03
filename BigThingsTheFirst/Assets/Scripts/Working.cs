c56ce145b2063c6b4a66c62f7400ee5dc49dca62
Chofmann512 <gymchris123@gmail.com>
2018-03-03 17:37:00 -0600

    StarGameSetup

---
+++ BigThingsTheFirst/Assets/Scripts/Working.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Working : MonoBehaviour {
+	public bool Hired;
+	// Use this for initialization
+	void Start () {
+		if (Hired)
+			Work ();
+	}
+	void Work(){
+		ShowUpEarly();
+		StayLate();
+		IncorportateAutonomousWorkflow();
+
+	}
+	void ShowUpEarly(){
+
+	}
+	void StayLate(){
+
+	}
+	void IncorportateAutonomousWorkflow(){
+
+	}
+
+}
