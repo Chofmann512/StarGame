ad63b97b4162f3c68cd3c6039a802640e6c2d0e0
Samuel Engert <maynard-2006@hotmail.com>
2018-03-04 21:29:40 -0600

    Added Asteroid.cs
    Added script to attach to Asteroid gameobjects, initially handles enabling the meteor's collider.

---
+++ BigThingsTheFirst/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.name != "AsteroidEnterTrigger"){
+			return;
+		}
+
+		gameObject.GetComponent<SphereCollider> ().isTrigger = false;
+		Destroy (this);
+	}
+}
