caa304640ff84519d5e50d0e00bb8cee750056e6
Samuel Engert <maynard-2006@hotmail.com>
2018-04-13 13:18:14 -0500

    Added GPGSDriver
    This script will be used to handle all functionality related to Google Play Services and its APIs. Also added a few test UI elements

---
+++ BigThingsTheFirst/Assets/Scripts/GPGSDriver.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.UI;
+
+public class GPGSDriver : MonoBehaviour {
+
+	[SerializeField]
+	private Text signInButtonText;
+	[SerializeField]
+	private Text authStatus;
+
+	// Use this for initialization
+	void Start () {
+		GameObject startButton = GameObject.Find ("startButton");
+		EventSystem.current.firstSelectedGameObject = startButton;
+
+		//Create GPG client
+		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+
+		//Set debugging output to true
+		PlayGamesPlatform.DebugLogEnabled = true;
+
+		//Initialize and activate the platform
+		PlayGamesPlatform.InitializeInstance(config);
+		PlayGamesPlatform.Activate ();
+	}
+
+	public void SignInCallback(bool success){
+		if (success) {
+			//login succeeded
+			Debug.Log ("(Star Swipe) Signed in!");
+			signInButtonText.text = "Sign out";
+
+			authStatus.text = "Signed in as: " + Social.localUser.userName;
+		} 
+		else {
+			//login failed
+			signInButtonText.text = "Sign in";
+			authStatus.text = "Sign-in failed";
+		}
+	}
+	
+	public  void SignIn(){
+		if(!PlayGamesPlatform.Instance.localUser.authenticated){
+			// TODO:
+			// Sign in with Play Game Services, showing the consent dialog
+			// by setting the second parameter to isSilent=false.
+			PlayGamesPlatform.Instance.Authenticate(SignInCallback, false);
+		}
+		else{
+			// Sign out of play games
+			PlayGamesPlatform.Instance.SignOut();
+
+			// Reset UI
+			signInButtonText.text = "Sign In";
+			authStatus.text = "";
+		}
+
+	}
+}
