7c254f998d53c7f4cfdef66b4f3243919662d509
Samuel Engert <maynard-2006@hotmail.com>
2018-04-16 18:22:21 -0500

    Added UIDriver.cs and more UI work

---
+++ BigThingsTheFirst/Assets/Scripts/UIDriver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDriver : MonoBehaviour {
+
+	[SerializeField]
+	private string facebookURL;
+	[SerializeField]
+	private string twitterURL;
+
+	public void LoadFacebookPage(){
+		if (facebookURL == "") {
+			Debug.LogError ("Missing a reference to the URL...");
+		} 
+		else {
+			Application.OpenURL (facebookURL);
+		}
+
+
+	}
+
+	public void LoadTwitterPage(){
+		if (twitterURL == "") {
+			Debug.LogError ("Missing a reference to the URL...");
+		} 
+		else {
+			Application.OpenURL (twitterURL);
+		}
+	}
+
+	public void LoadMainMenu(){
+		
+	}
+}
