4acf528a0b870eb975724412f93512f2696be593
Samuel Engert <maynard-2006@hotmail.com>
2018-04-26 22:17:16 -0500

    Imported GoogleMobileAds Plugin and added preloader/ad framework
    -Application now opens with the scene "Preloader", in this scene a persistant AdDriver is created. For this push it contains the implementation for a banner ad.

---
+++ BigThingsTheFirst/Assets/Scripts/AdDriver.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;//https://developers.google.com/admob/unity/start
+
+public class AdDriver : MonoBehaviour {
+
+	public static AdDriver Instance{ set; get;}
+
+	public string appId;//Android app id, need another one for IOs
+	public string bannerId;
+	public string interstitialId;
+
+	private BannerView bannerView;//From GoogleMobileAds.Api
+
+	void Awake () {
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+
+		//#if UNITY_EDITOR
+		//Debug.Log("Failed to initialize MobileAds. Can not use Ads in editor.");
+		//#elif UNITY_ANDROID
+		MobileAds.Initialize(appId);
+
+
+		//request a banner
+		this.RequestBannerAd();
+	}
+	
+	private void RequestBannerAd(){
+		//Create a BannerView
+		bannerView = new BannerView (bannerId, AdSize.Banner, AdPosition.Bottom);
+
+		//Request an ad
+		AdRequest req = new AdRequest.Builder().Build();
+
+		//Load the banner with the request
+		bannerView.LoadAd(req);
+	}
+}
---
+++ BigThingsTheFirst/Assets/Scripts/Preloader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Preloader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Enter main game loop
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+}
