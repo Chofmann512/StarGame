a19e36548e64407681d6ccfb7dc314f257b0d163
Chofmann512 <gymchris123@gmail.com>
2018-04-28 23:15:49 -0500

    SpriteScroll

---
+++ BigThingsTheFirst/Assets/Scripts/SpriteScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SpriteScroll : MonoBehaviour {
+	public float ScrollX = .5f;
+	public float ScrollY = .5f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float OffsetX = Time.time * ScrollX;
+		float OffsetY = Time.time * ScrollY;
+		GetComponent<RawImage>().material.mainTextureOffset= new Vector2 (OffsetX, OffsetY);
+	}
+}
