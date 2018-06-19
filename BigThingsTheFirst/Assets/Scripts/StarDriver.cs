b60b258a468c37c7f1766c693324f59661f158f0
Chofmann512 <gymchris123@gmail.com>
2018-06-19 17:11:51 -0500

    FinallyFixedLerpHopefully

--- BigThingsTheFirst/Assets/Scripts/StarDriver.cs
+++ BigThingsTheFirst/Assets/Scripts/StarDriver.cs
@@ -19,6 +19,8 @@ public class StarDriver : MonoBehaviour {
     public GameObject soundEffectManager;
     public UIDriver uiDriver;
 
+    public static bool bankShotted;
+
 	// Use this for initialization
 	void Start () {
 		flickBool = false;
@@ -36,6 +38,7 @@ public class StarDriver : MonoBehaviour {
 			if (Vector3.Distance(v3, startFlick.position) < maxDistanceGrab) {
 				gameObject.GetComponent<Rigidbody> ().velocity = Vector3.zero;
 				CancelInvoke ("cancelBankShot");
+                if(!GameDriver.lerping)
 				bankShot = 1;
                 
 
@@ -96,15 +99,23 @@ public class StarDriver : MonoBehaviour {
 
 	public void BankShot(){
 		bankShot = 2;
+        bankShotted = true;
 		CancelInvoke ("cancelBankShot");
 		Invoke ("cancelBankShot", 1.4f);
         
         //callNum
 	}
 	public void cancelBankShot(){
-		
-		bankShot = 1;
-        
+        if (GameDriver.lerping == false)
+        {
+            bankShot = 1;
+            uiDriver.UpdateMultiplierText(StarDriver.bankShot);
+        }
+        else
+        {
+            bankShot = 2;
+        }
+        bankShotted = false;
         
         //callNum
     }
