21b2810a597ea0fe772c54da2073a57b8e69bff0
Samuel Engert <maynard-2006@hotmail.com>
2018-06-19 15:56:32 -0500

    Revert "BankShot"
    This reverts commit f5d2b703b69d0435a2b5d97cd343e88b280cd82e.

--- BigThingsTheFirst/Assets/Scripts/GameDriver.cs
+++ BigThingsTheFirst/Assets/Scripts/GameDriver.cs
@@ -24,12 +24,9 @@ public class GameDriver : MonoBehaviour {
 	public AudioSource scoreCount;
     public AudioSource menuMusic;
     public AudioSource gameMusic;
-<<<<<<< HEAD
     public AudioSource asteroidCapture;
     public AudioSource asteroidSpawn;
-=======
     public AudioSource AsteroidCapture;
->>>>>>> parent of eee83fb... Fixes to Pause/Unpause
     public GameObject musicManager;
     public GameObject soundEffectManager;
     public UIDriver uiDriver;
